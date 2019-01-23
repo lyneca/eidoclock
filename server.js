95dda561dc0bcba91a9b4cc429c26eff4870d6b4
Lyneca <atraguliauniliantauthr@gmail.com>
2019-01-23 17:50:36 +1100

    Heroku pls -_-

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var static = require('node-static');
  
 var fileServer = new static.Server('./build');
+
+console.log(process.env.PORT);
  
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
