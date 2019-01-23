20ac178c440307b5ca3fd48c4bcbc01b022b0aa1
Lyneca <atraguliauniliantauthr@gmail.com>
2019-01-24 00:54:24 +1100

    Updates

---
+++ precache-manifest.56a73b937c090a074c89897db5bbcacc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e95678778b797e53410",
+    "url": "/static/css/main.72bb01bd.chunk.css"
+  },
+  {
+    "revision": "1e95678778b797e53410",
+    "url": "/static/js/main.1e956787.chunk.js"
+  },
+  {
+    "revision": "deebe173955dcee70ec1",
+    "url": "/static/js/1.deebe173.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "055d4c5c2dd02748b89a5493007eaca6",
+    "url": "/static/media/morning.055d4c5c.png"
+  },
+  {
+    "revision": "ae164e7aaba68b9381cfbf6d7f2fe871",
+    "url": "/static/media/day.ae164e7a.png"
+  },
+  {
+    "revision": "3d501728d8e3da05fc07dbb0d762c325",
+    "url": "/static/media/evening.3d501728.png"
+  },
+  {
+    "revision": "3c4f2bfc906d47811bf3106d7b69ea86",
+    "url": "/static/media/night.3c4f2bfc.png"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/static/media/open_sans.60c86674.woff2"
+  },
+  {
+    "revision": "29b00ebcf93fda46ac9957fa4816eafd",
+    "url": "/static/media/inconsolata.29b00ebc.ttf"
+  },
+  {
+    "revision": "2cb777cb2d55e9fe45eddd562b2312d3",
+    "url": "/index.html"
+  }
+];
